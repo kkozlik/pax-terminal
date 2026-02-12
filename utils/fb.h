1bee93270661c435c863890688d0a458ed1b2347
Karel Kozlík <kozlik@ok1kpi.cz>
2026-02-12 17:43:37 +0100

    Adding constants for some colors

--- utils/fb.h
+++ utils/fb.h
@@ -12,6 +12,13 @@
 
 typedef unsigned short color_t;
 
+static const color_t color_blue   = 0x001F;
+static const color_t color_red    = 0xF800;
+static const color_t color_yellow = 0xFFE0;
+static const color_t color_black  = 0x0000;
+static const color_t color_white  = 0xFFFF;
+
+
 color_t *fb_init(int *out_fd);
 
 void fb_close(color_t *fbp,int fb_fd);
